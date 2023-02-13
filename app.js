34b69eb41da399af8466cb7f98766749112c89fd
vanessa duval <vanessadevetudiante@gmail.com>
2023-02-13 19:04:38 +0100

    aze

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://vanessa:h1nFPvQNtnKagnQO@cluster0.aebfvkd.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -24,5 +25,6 @@ app.use((req, res, next) => {
   app.use(bodyParser.json())
 
   app.use('/api/stuff', stuffRoutes);
+  app.use('/api/auth', userRoutes);
 
   module.exports = app;
